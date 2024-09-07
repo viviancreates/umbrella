55e99e0f3e1a2249f5350dd2d09b28217c987786
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-09-07 04:43:58 +0000

    Edits to gmaps key

--- umbrella.rb
+++ umbrella.rb
@@ -8,6 +8,16 @@ user_location = gets.chomp
 
 pp user_location
 
+# Original pasted looks like https://maps.googleapis.com/maps/api/geocode/json?address=Merchandise%20Mart%20Chicago&key=jdijxidjYADAYADAthisWASanAPICODE
+# This is turned to below
+# maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=Merchandise%20Mart%20Chicago&key="+ ENV.fetch("GMAPS_KEY")
+
+# HOWEVER, we want to NOT hardcode location too, so...
+# add the user input variable 
+maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
+
+pp maps_url
+
 pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
 
 # Assemble url string using website and api variable
