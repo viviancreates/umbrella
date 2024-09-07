beb320cfd00ddfdc1f0c5e0cab72a4892bfa2653
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-09-07 04:28:58 +0000

    Add api key var and start user prompts

--- umbrella.rb
+++ umbrella.rb
@@ -1,4 +1,19 @@
 require "http"
 require "json"
 
+pp "Hello!"
+pp "Where are you located?"
+
+user_location = gets.chomp
+
+pp user_location
+
 pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
+
+# Assemble url string using website and api variable
+pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887, -87.6355"
+
+# GET request for URL
+raw_response = HTTP.get(pirate_weather_url)
+
+parsed_response = JSON.parse(raw_response)
